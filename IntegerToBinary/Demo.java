c08cff8dd5ca5797c2f2ff3b6b5ee5995a7ebebf
anshul <anshulvishwakarma830@gmail.com>
2024-05-23 10:52:26 +0530

    Convert integer to binary

---
+++ IntegerToBinary/Demo.java
@@ -0,0 +1,9 @@
+public class Demo {
+    public static void main(String[] args) {
+        int val = 15;
+        // integer
+        System.out.println("Integer: " + val);
+        // binary
+        System.out.println("Binary = " + Integer.toBinaryString(val));
+    }
+}
